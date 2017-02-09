9ed531258adc8f933c5d9d4d4b6ff18d10849009
Barry Wallis <barry_wallis@acm.org>
2017-02-08 22:09:29 -0800

    Add code to change icon by keypress and draw color lines.

--- Mouse/MainWindow.xaml.cs
+++ Mouse/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,7 @@ namespace Mouse
     {
         double previousX = 0;
         double previousY = 0;
+        SolidColorBrush brush;
 
         public MainWindow()
         {
@@ -33,8 +35,9 @@ namespace Mouse
             if (e is null)
                 throw new ArgumentNullException(nameof(e));
 
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if (e.LeftButton == MouseButtonState.Pressed || e.RightButton == MouseButtonState.Pressed)
             {
+                Debug.Assert(brush != null);
                 double x = e.MouseDevice.GetPosition(this).X;
                 double y = e.MouseDevice.GetPosition(this).Y;
                 Line line = new Line()
@@ -44,7 +47,7 @@ namespace Mouse
                     X2 = x,
                     Y2 = y,
                     SnapsToDevicePixels = true,
-                    Stroke = Brushes.Black,
+                    Stroke = brush,
                     StrokeThickness = 1,
                 };
                 Canvas.Children.Add(line);
@@ -57,6 +60,391 @@ namespace Mouse
         {
             previousX = e.MouseDevice.GetPosition(this).X;
             previousY = e.MouseDevice.GetPosition(this).Y;
+            Color color = new Color()
+            {
+                A = 255,
+                R = 255,
+                G = 0,
+                B = 0,
+            };
+            brush = new SolidColorBrush(color);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e is null)
+                throw new ArgumentNullException(nameof(e));
+
+            switch (e.Key)
+            {
+                case Key.None:
+                    break;
+                case Key.Cancel:
+                    break;
+                case Key.Back:
+                    break;
+                case Key.Tab:
+                    break;
+                case Key.LineFeed:
+                    break;
+                case Key.Clear:
+                    break;
+                case Key.Enter:
+                    break;
+                case Key.Pause:
+                    break;
+                case Key.CapsLock:
+                    break;
+                case Key.KanaMode:
+                    break;
+                case Key.JunjaMode:
+                    break;
+                case Key.FinalMode:
+                    break;
+                case Key.KanjiMode:
+                    break;
+                case Key.Escape:
+                    break;
+                case Key.ImeConvert:
+                    break;
+                case Key.ImeNonConvert:
+                    break;
+                case Key.ImeAccept:
+                    break;
+                case Key.ImeModeChange:
+                    break;
+                case Key.Space:
+                    break;
+                case Key.PageUp:
+                    break;
+                case Key.PageDown:
+                    break;
+                case Key.End:
+                    break;
+                case Key.Home:
+                    break;
+                case Key.Left:
+                    break;
+                case Key.Up:
+                    break;
+                case Key.Right:
+                    break;
+                case Key.Down:
+                    break;
+                case Key.Select:
+                    break;
+                case Key.Print:
+                    break;
+                case Key.Execute:
+                    break;
+                case Key.PrintScreen:
+                    break;
+                case Key.Insert:
+                    break;
+                case Key.Delete:
+                    break;
+                case Key.Help:
+                    break;
+                case Key.D0:
+                    break;
+                case Key.D1:
+                    break;
+                case Key.D2:
+                    break;
+                case Key.D3:
+                    break;
+                case Key.D4:
+                    break;
+                case Key.D5:
+                    break;
+                case Key.D6:
+                    break;
+                case Key.D7:
+                    break;
+                case Key.D8:
+                    break;
+                case Key.D9:
+                    break;
+                case Key.A:
+                    (sender as Window).Cursor = Cursors.Arrow;
+                    break;
+                case Key.B:
+                    (sender as Window).Cursor = Cursors.IBeam;
+                    break;
+                case Key.C:
+                    (sender as Window).Cursor = Cursors.Wait;
+                    break;
+                case Key.D:
+                    break;
+                case Key.E:
+                    break;
+                case Key.F:
+                    break;
+                case Key.G:
+                    break;
+                case Key.H:
+                    break;
+                case Key.I:
+                    break;
+                case Key.J:
+                    break;
+                case Key.K:
+                    break;
+                case Key.L:
+                    break;
+                case Key.M:
+                    break;
+                case Key.N:
+                    break;
+                case Key.O:
+                    break;
+                case Key.P:
+                    break;
+                case Key.Q:
+                    break;
+                case Key.R:
+                    break;
+                case Key.S:
+                    break;
+                case Key.T:
+                    break;
+                case Key.U:
+                    break;
+                case Key.V:
+                    break;
+                case Key.W:
+                    break;
+                case Key.X:
+                    (sender as Window).Cursor = Cursors.Arrow;
+                    Close();
+                    break;
+                case Key.Y:
+                    break;
+                case Key.Z:
+                    break;
+                case Key.LWin:
+                    break;
+                case Key.RWin:
+                    break;
+                case Key.Apps:
+                    break;
+                case Key.Sleep:
+                    break;
+                case Key.NumPad0:
+                    break;
+                case Key.NumPad1:
+                    break;
+                case Key.NumPad2:
+                    break;
+                case Key.NumPad3:
+                    break;
+                case Key.NumPad4:
+                    break;
+                case Key.NumPad5:
+                    break;
+                case Key.NumPad6:
+                    break;
+                case Key.NumPad7:
+                    break;
+                case Key.NumPad8:
+                    break;
+                case Key.NumPad9:
+                    break;
+                case Key.Multiply:
+                    break;
+                case Key.Add:
+                    break;
+                case Key.Separator:
+                    break;
+                case Key.Subtract:
+                    break;
+                case Key.Decimal:
+                    break;
+                case Key.Divide:
+                    break;
+                case Key.F1:
+                    break;
+                case Key.F2:
+                    break;
+                case Key.F3:
+                    break;
+                case Key.F4:
+                    break;
+                case Key.F5:
+                    break;
+                case Key.F6:
+                    break;
+                case Key.F7:
+                    break;
+                case Key.F8:
+                    break;
+                case Key.F9:
+                    break;
+                case Key.F10:
+                    break;
+                case Key.F11:
+                    break;
+                case Key.F12:
+                    break;
+                case Key.F13:
+                    break;
+                case Key.F14:
+                    break;
+                case Key.F15:
+                    break;
+                case Key.F16:
+                    break;
+                case Key.F17:
+                    break;
+                case Key.F18:
+                    break;
+                case Key.F19:
+                    break;
+                case Key.F20:
+                    break;
+                case Key.F21:
+                    break;
+                case Key.F22:
+                    break;
+                case Key.F23:
+                    break;
+                case Key.F24:
+                    break;
+                case Key.NumLock:
+                    break;
+                case Key.Scroll:
+                    break;
+                case Key.LeftShift:
+                    break;
+                case Key.RightShift:
+                    break;
+                case Key.LeftCtrl:
+                    break;
+                case Key.RightCtrl:
+                    break;
+                case Key.LeftAlt:
+                    break;
+                case Key.RightAlt:
+                    break;
+                case Key.BrowserBack:
+                    break;
+                case Key.BrowserForward:
+                    break;
+                case Key.BrowserRefresh:
+                    break;
+                case Key.BrowserStop:
+                    break;
+                case Key.BrowserSearch:
+                    break;
+                case Key.BrowserFavorites:
+                    break;
+                case Key.BrowserHome:
+                    break;
+                case Key.VolumeMute:
+                    break;
+                case Key.VolumeDown:
+                    break;
+                case Key.VolumeUp:
+                    break;
+                case Key.MediaNextTrack:
+                    break;
+                case Key.MediaPreviousTrack:
+                    break;
+                case Key.MediaStop:
+                    break;
+                case Key.MediaPlayPause:
+                    break;
+                case Key.LaunchMail:
+                    break;
+                case Key.SelectMedia:
+                    break;
+                case Key.LaunchApplication1:
+                    break;
+                case Key.LaunchApplication2:
+                    break;
+                case Key.OemSemicolon:
+                    break;
+                case Key.OemPlus:
+                    break;
+                case Key.OemComma:
+                    break;
+                case Key.OemMinus:
+                    break;
+                case Key.OemPeriod:
+                    break;
+                case Key.OemQuestion:
+                    break;
+                case Key.OemTilde:
+                    break;
+                case Key.AbntC1:
+                    break;
+                case Key.AbntC2:
+                    break;
+                case Key.OemOpenBrackets:
+                    break;
+                case Key.OemPipe:
+                    break;
+                case Key.OemCloseBrackets:
+                    break;
+                case Key.OemQuotes:
+                    break;
+                case Key.Oem8:
+                    break;
+                case Key.OemBackslash:
+                    break;
+                case Key.ImeProcessed:
+                    break;
+                case Key.System:
+                    break;
+                case Key.OemAttn:
+                    break;
+                case Key.OemFinish:
+                    break;
+                case Key.OemCopy:
+                    break;
+                case Key.OemAuto:
+                    break;
+                case Key.OemEnlw:
+                    break;
+                case Key.OemBackTab:
+                    break;
+                case Key.Attn:
+                    break;
+                case Key.CrSel:
+                    break;
+                case Key.ExSel:
+                    break;
+                case Key.EraseEof:
+                    break;
+                case Key.Play:
+                    break;
+                case Key.Zoom:
+                    break;
+                case Key.NoName:
+                    break;
+                case Key.Pa1:
+                    break;
+                case Key.OemClear:
+                    break;
+                case Key.DeadCharProcessed:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            previousX = e.MouseDevice.GetPosition(this).X;
+            previousY = e.MouseDevice.GetPosition(this).Y;
+            Color color = new Color()
+            {
+                A = 255,
+                R = 0,
+                G = 0,
+                B = 255,
+            };
+            brush = new SolidColorBrush(color);
         }
     }
 }
