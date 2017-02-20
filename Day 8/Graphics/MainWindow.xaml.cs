7a0b7be3d0ebf9787b509a424e88657c22229a25
Barry Wallis <barry_wallis@acm.org>
2017-02-19 16:54:48 -0800

    Day 8: Graphics

---
+++ Day 8/Graphics/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Graphics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PaintWindow paintWindow = new PaintWindow();
+        private SolidColorBrush brush = null;
+        private System.Windows.Controls.Image image = null;
+        private RadioButton currentToolRadioButton = null;
+
+        private readonly SolidColorBrush blackBrush = new SolidColorBrush(Colors.Black);
+        private readonly SolidColorBrush blueBrush = new SolidColorBrush(Colors.Blue);
+        private readonly SolidColorBrush greenBrush = new SolidColorBrush(Colors.Green);
+        private readonly SolidColorBrush cyanBrush = new SolidColorBrush(Colors.Cyan);
+        private readonly SolidColorBrush redBrush = new SolidColorBrush(Colors.Red);
+        private readonly SolidColorBrush magentaBrush = new SolidColorBrush(Colors.Magenta);
+        private readonly SolidColorBrush yellowBrush = new SolidColorBrush(Colors.Yellow);
+        private readonly SolidColorBrush whiteBrush = new SolidColorBrush(Colors.White);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Activated += OnActivated;
+            paintWindow.Show();
+            currentToolRadioButton = PenRadioButton;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OnActivated(object sender, EventArgs eventArgs)
+        {
+            paintWindow.Owner = this;
+            Activated -= OnActivated;
+        }
+
+        private void ColorChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is null)
+                throw new ArgumentNullException(nameof(sender));
+
+            RadioButton radioButton = sender as RadioButton;
+            Debug.Assert(sender != null);
+
+            switch (radioButton.Name)
+            {
+                case "BlackRadioButton":
+                    brush = blackBrush;
+                    break;
+                case "BlueRadioButton":
+                    brush = blueBrush;
+                    break;
+                case "GreenRadioButton":
+                    brush = greenBrush;
+                    break;
+                case "CyanRadioButton":
+                    brush = cyanBrush;
+                    break;
+                case "RedRadioButton":
+                    brush = redBrush;
+                    break;
+                case "MagentaRadioButton":
+                    brush = magentaBrush;
+                    break;
+                case "YellowRadioButton":
+                    brush = yellowBrush;
+                    break;
+                case "WhiteRadioButton":
+                    brush = whiteBrush;
+                    break;
+                default:
+                    Debug.Fail("Unexpected brush found");
+                    break;
+            }
+
+            Draw();
+        }
+
+        private void ShapeChanged(object sender, RoutedEventArgs e)
+        {
+            Draw();
+        }
+
+        private void ToolChanged(object sender, RoutedEventArgs e)
+        {
+            Draw();
+            currentToolRadioButton = sender as RadioButton;
+            Debug.Assert(currentToolRadioButton != null);
+        }
+
+        private void Draw()
+        {
+            if (LineRadioButton is null)
+                return;
+
+            if (BitmapRadioButton.IsChecked == true)
+            {
+                if (image is null)
+                {
+                    MessageBox.Show("No image file selected");
+                    currentToolRadioButton.IsChecked = true;
+                }
+                else
+                    paintWindow.DrawImage(image);
+            }
+            else if (LineRadioButton.IsChecked == true)
+                paintWindow.DrawLines(brush);
+            else if (SquareRadioButton.IsChecked == true)
+                paintWindow.DrawShapes(brush, TheShape.Square, BrushRadioButton.IsChecked == true);
+            else if (CircleRadioButton.IsChecked == true)
+                paintWindow.DrawShapes(brush, TheShape.Circle, BrushRadioButton.IsChecked == true);
+            else
+                Debug.Fail("Shape radio button not found");
+        }
+
+        private void BitmapButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                DefaultExt = ".bmp",
+                Filter = "Image files|*.bmp;*.gif;*.ico;*.jpg;*.png;*.wdp;*tiff",
+                InitialDirectory = "Pictures",
+                CheckPathExists = true,
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                image = new System.Windows.Controls.Image();
+                BitmapImage sourceImage = new BitmapImage();
+                sourceImage.BeginInit();
+                sourceImage.UriSource = new Uri(openFileDialog.FileName);
+                sourceImage.EndInit();
+                image.Source = sourceImage;
+                image.Stretch = Stretch.Uniform;
+                image.Height = paintWindow.Height;
+                image.Width = paintWindow.Width;
+                Draw();
+            }
+        }
+    }
+}
---
+++ Day 8/Graphics/PaintWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Graphics
+{
+    /// <summary>
+    /// Interaction logic for PaintWindow.xaml
+    /// </summary>
+    public partial class PaintWindow : Window
+    {
+        private DashStyle[] dashStyles = new DashStyle[] { DashStyles.Solid, DashStyles.Dot, DashStyles.Dash, DashStyles.DashDot, DashStyles.DashDotDot };
+
+        public PaintWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Draw lines in each possible dast sytle.
+        /// </summary>
+        /// <param name="brush">The brush to use to draw the lines.</param>
+        public void DrawLines(SolidColorBrush brush)
+        {
+            Canvas.Children.Clear();
+            System.Windows.Point start = new System.Windows.Point(0, 0);
+            double distance = Height / (dashStyles.Length + 1);
+            System.Windows.Point end = new System.Windows.Point(Width, start.Y);
+            foreach (DashStyle dashStyle in dashStyles)
+            {
+                start = new System.Windows.Point(start.X, start.Y + distance);
+                end = new System.Windows.Point(end.X, start.Y);
+                DrawLine(brush, ref start, ref end, dashStyle);
+            }
+        }
+
+        /// <summary>
+        /// Draw shapes using each dash style as the black border.
+        /// </summary>
+        /// <param name="brush">The brush to use for each square.</param>
+        public void DrawShapes(SolidColorBrush brush, TheShape shape, bool isFilled)
+        {
+            Canvas.Children.Clear();
+            double vertical = Height / 2;
+            double height = vertical - 10;
+            double horizontal = Width / (dashStyles.Length + 1);
+            double width = horizontal - 10;
+            double top = 5;
+            double left = 5;
+            double bottom = top + height;
+            double right = left + width;
+            foreach (DashStyle dashStyle in dashStyles)
+            {
+                switch (shape)
+                {
+                    case TheShape.Square:
+                        DrawShape(new System.Windows.Shapes.Rectangle(), brush, top, left, bottom, right, dashStyle, isFilled);
+                        break;
+                    case TheShape.Circle:
+                        DrawShape(new Ellipse(), brush, top, left, bottom, right, dashStyle, isFilled);
+                        break;
+                    default:
+                        break;
+                }
+                left += horizontal;
+                right = left + width;
+            }
+        }
+
+        /// <summary>
+        /// Draw a shape with the given properties.
+        /// </summary>
+        /// <param name="brush">The brush to use to fill the shape.</param>
+        /// <param name="top">The location of the top of the shape.</param>
+        /// <param name="left">The location of the left side of the shape.</param>
+        /// <param name="bottom">The location of the bottom of the shape.</param>
+        /// <param name="right">The location of the right side of the shape.</param>
+        /// <param name="dashStyle">The dash style of the black border.</param>
+        /// <param name="isFilled">If true, fill with the brush; otherwise outline dashes with the brush.</param>
+        private void DrawShape(Shape shape, SolidColorBrush brush, double top, double left, double bottom, double right, DashStyle dashStyle, bool isFilled)
+        {
+            shape.Width = right - left;
+            shape.Height = bottom - top;
+            if (isFilled)
+                shape.Fill = brush;
+            else
+            {
+                shape.Stroke = brush;
+                shape.StrokeThickness = 1;
+                shape.StrokeDashArray = dashStyle.Dashes;
+                shape.StrokeDashCap = PenLineCap.Round;
+            }
+
+            Canvas.Children.Add(shape);
+            Canvas.SetLeft(shape, left);
+            Canvas.SetTop(shape, top);
+        }
+
+        public void DrawImage(System.Windows.Controls.Image image)
+        {
+            Canvas.Children.Clear();
+            Canvas.Children.Add(image);
+        }
+
+        /// <summary>
+        /// Draw a line.
+        /// </summary>
+        /// <param name="brush">The brush to use for the line.</param>
+        /// <param name="start">The starting point for the line.</param>
+        /// <param name="end">The ending point of the line.</param>
+        /// <param name="dashStyle">The dash style for the line.</param>
+        private void DrawLine(SolidColorBrush brush, ref System.Windows.Point start, ref System.Windows.Point end, DashStyle dashStyle)
+        {
+            Line line = new Line()
+            {
+                X1 = start.X,
+                Y1 = start.Y,
+                X2 = end.X,
+                Y2 = end.Y,
+                SnapsToDevicePixels = true,
+                Stroke = brush,
+                StrokeThickness = 5,
+                StrokeDashArray = dashStyle.Dashes,
+                StrokeDashCap = PenLineCap.Round,
+            };
+            Canvas.Children.Add(line);
+        }
+    }
+}
